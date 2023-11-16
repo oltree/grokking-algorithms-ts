5243e7a60a43acac1dc19c7e75b733f25aa74812
oltree <oleg.melekh.frontend@gmail.com>
2023-11-16 20:43:40 +0300

    feat; add fn fibonacci

--- tasks.js
+++ tasks.js
@@ -164,3 +164,19 @@ const reslut2 = (arr) => {
 
   return countMap;
 };
+
+//фибоначчи
+const fibonacci = (num) => {
+  const result = [0, 1];
+
+  for (let i = 2; i <= num; i++) {
+    result.push(result[i - 1] + result[i - 2]);
+  }
+
+  return result[num];
+  /* if (num < 2) {
+    return num;
+  }
+
+  return fibonacci(num - 1) + fibonacci(num - 2); */
+};
