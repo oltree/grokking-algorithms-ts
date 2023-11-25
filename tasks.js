cf8638d4e062a0c032a1224d8857ea6b342bd1c2
oltree <oleg.melekh.frontend@gmail.com>
2023-11-25 20:51:47 +0300

    feat: add isPrime fn

--- tasks.js
+++ tasks.js
@@ -223,3 +223,16 @@ const getCurrencies = (arr) => {
     return acc;
 }, {}) */
 };
+
+//простые числа
+const isPrime = (num) => {
+  const max = Math.sqrt(num);
+
+  for (let i = 2; i <= max; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return num > 1;
+};
