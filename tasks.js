7d37c2ad4b0251b5336a8a9c287cbbe25e9b0d19
oltree <oleg.melekh.frontend@gmail.com>
2023-12-01 21:00:17 +0300

    feat: add fn

--- tasks.js
+++ tasks.js
@@ -283,3 +283,20 @@ const singleNumber = (nums) => {
 
   return uniqSum * 2 - numSum;
 };
+
+//числовые паиндромы
+const isPalindromeNumber = (x) => {
+  if (x < 0) return false;
+  if (x < 10) return true;
+  if (x % 10 === 0) return false;
+
+  let rev = 0;
+
+  while (x > rev) {
+    rev *= 10;
+    rev += x % 10;
+    x = Math.trunc(x / 10);
+  }
+
+  return x === rev || x === Math.trunc(rev / 10);
+};
